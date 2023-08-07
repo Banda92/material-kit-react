10961002c34edd3e5383123eb8cc01d095809ca2
Banda92 <qksdudgns92@gmail.com>
2023-08-07 14:02:57 +0900

    테이블2업데이트 완료
    바차트 적용
    참고: scssmodule은 서버 실행시킬때 기준으로 컴파일되는것으로 보임.

--- src/components/barChartByh.js
+++ src/components/barChartByh.js
@@ -1,82 +1,66 @@
+/* BarChartByh.js */
+
 import React from "react";
 import { Card, CardContent, CardHeader } from "@mui/material";
-import {styls} from './barChartByh.module.scss';
+import styles from "./BarChartByh.module.scss"; // SCSS 파일을 import 합니다.
 
 const BarChartByh = (props) => {
   const data = props.data;
   const { sx } = props;
-  
 
   return (
     <Card sx={sx}>
       <CardHeader
         title="테이블2"
         sx={{
-            paddingTop: '10px',
-            paddingBottom:'0px',
-          }}
+          paddingTop: "10px",
+          paddingBottom: "0px",
+        }}
       />
-      <CardContent sx={{
-                paddingTop:"10px"
-              }}
-              >
-
-          <div className="Chart">
-            <table
-              style={{
-                width: "100%",
-                height: "100%",
-                borderCollapse: "collapse",
-                alignItems: "center",
-                alignSelf: "center",
-                alignContent: "center",
-                fontSize:"0.4rem",
-              }}
-            >
-              <thead>
-                <tr>
-                  <th
-                    style={{
-                      width: "10%",
-                      backgroundColor: "#3858ed",
-                      fontWeight: "bold",
-                      color: "white",
-                    }}
-                  >
-                    병동
-                  </th>
-                  <th style={{ backgroundColor: "#3858ed", fontWeight: "bold", color: "white" }}>
-                    병상이용율
-                  </th>
+      <CardContent
+        sx={{
+          paddingTop: "10px",
+        }}
+      >
+        <div className={styles.Chart}>
+          {" "}
+          {/* SCSS로 변경한 부분 */}
+          <table className={styles.table}>
+            {/* SCSS로 변경한 부분 */}
+            <thead>
+              <tr>
+                <th className={styles.th}>병동</th> {/* SCSS로 변경한 부분 */}
+                <th className={styles.th}>병상이용율</th> {/* SCSS로 변경한 부분 */}
+              </tr>
+            </thead>
+            <tbody className={styles.tbody}>
+              {" "}
+              {/* SCSS로 변경한 부분 */}
+              {data.map((row, index) => (
+                <tr key={index}>
+                  <td className={styles.td}>{row.Ward}</td> {/* SCSS로 변경한 부분 */}
+                  <td className={styles.td}>
+                    {" "}
+                    {/* SCSS로 변경한 부분 */}
+                    <div className={styles.barContainer}>
+                      <div
+                        className={styles.bar}
+                        style={{
+                          width: `${row.OccupancyRate * 100}%`,
+                        }}
+                      ></div>
+                      {/* SCSS로 변경한 부분 */}
+                      <div className={styles.barText}>
+                        {Math.round(row.OccupancyRate * 100)}%
+                      </div>{" "}
+                      {/* SCSS로 변경한 부분 */}
+                    </div>
+                  </td>
                 </tr>
-              </thead>
-              <tbody style={{textAlign:"center"}}>
-                {data.map((row, index) => (
-                  <tr key={index}>
-                    <td style={{ width: "13%", padding: "3px", border: "1px solid lightgray" }}>
-                      {row.Ward}
-                    </td>
-                    <td style={{ padding: "3px", border: "1px solid lightgray" }}>
-                      <div style={{ display: "flex" }}>
-                        <div
-                          className="bar"
-                          style={{
-                            height: "10px",
-                            backgroundColor: "orange",
-                            borderRadius: "0px 10px 10px 0px",
-                            width: `${row.OccupancyRate * 100}%`,
-                          }}
-                        ></div>
-                        <div style={{ marginLeft: "10px", lineHeight: "10px" }}>
-                          {Math.round(row.OccupancyRate * 100)}%
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </CardContent>
     </Card>
   );
--- src/components/tableChartByh.js
+++ src/components/tableChartByh.js
@@ -1,125 +1,47 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
-
+import styles from "./TableChartByh.module.scss";
 
 const TableChartByh = (props) => {
-    const { data, sx } = props;
+  const { data, sx } = props;
 
   return (
     <Card sx={sx}>
-        <CardHeader title="테이블1"
+      <CardHeader
+        title="테이블1"
         sx={{
-            paddingTop: '10px',
-            paddingBottom:'0px',
-          }}
-        />
-              <CardContent sx={{
-                paddingTop:"10px",
-              }}
-              >
-          <table
-            style={{
-              width: "100%",
-              borderCollapse: "collapse",
-              fontSize:"0.4rem",
-              textAlign:"center",
-              
-            }}
-          >
-            <thead>
-              <tr>
-                <th
-                  style={{
-                    padding: "3px",
-                    border: "1px solid lightgray",
-                    fontWeight: "bold",
-                    backgroundColor: "#3858ed",
-                    color: "white",
-                  }}
-                >
-                  병동
-                </th>
-                <th
-                  style={{
-                    padding: "3px",
-                    border: "1px solid lightgray",
-                    fontWeight: "bold",
-                    backgroundColor: "#3858ed",
-                    color: "white",
-                  }}
-                >
-                  총원
-                </th>
-                <th
-                  style={{
-                    padding: "3px",
-                    border: "1px solid lightgray",
-                    fontWeight: "bold",
-                    backgroundColor: "#3858ed",
-                    color: "white",
-                  }}
-                >
-                  재원
-                </th>
-                <th
-                  style={{
-                    padding: "3px",
-                    border: "1px solid lightgray",
-                    fontWeight: "bold",
-                    backgroundColor: "#3858ed",
-                    color: "white",
-                  }}
-                >
-                  입원예정
-                </th>
-                <th
-                  style={{
-                    padding: "3px",
-                    border: "1px solid lightgray",
-                    fontWeight: "bold",
-                    backgroundColor: "#3858ed",
-                    color: "white",
-                  }}
-                >
-                  퇴원예정
-                </th>
-                <th
-                  style={{
-                    padding: "3px",
-                    border: "1px solid lightgray",
-                    fontWeight: "bold",
-                    backgroundColor: "#3858ed",
-                    color: "white",
-                  }}
-                >
-                  공병상
-                </th>
+          paddingTop: '10px',
+          paddingBottom: '0px',
+        }}
+      />
+      <CardContent
+        sx={{
+          paddingTop: "10px",
+        }}
+      >
+        <table className={styles.customTable}>
+          <thead>
+            <tr>
+              <th className={styles.tableHeader}>병동</th>
+              <th className={styles.tableHeader}>총원</th>
+              <th className={styles.tableHeader}>재원</th>
+              <th className={styles.tableHeader}>입원예정</th>
+              <th className={styles.tableHeader}>퇴원예정</th>
+              <th className={styles.tableHeader}>공병상</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td className={styles.tableCell}>{row.Ward}</td>
+                <td className={styles.tableCell}>{row.TotalBed}</td>
+                <td className={styles.tableCell}>{row.InHospital}</td>
+                <td className={`${styles.tableCell} ${styles.willAdmission}`}>{row.WillAdminssion}</td>
+                <td className={`${styles.tableCell} ${styles.willDischarged}`}>{row.WillDischarged}</td>
+                <td className={styles.tableCell}>{row.EmptyBed}</td>
               </tr>
-            </thead>
-            <tbody>
-              {data.map((row, index) => (
-                <tr key={index}>
-                  <td style={{ padding: "3px", border: "1px solid lightgray" }}>{row.Ward}</td>
-                  <td style={{ padding: "3px", border: "1px solid lightgray" }}>{row.TotalBed}</td>
-                  <td style={{ padding: "3px", border: "1px solid lightgray" }}>
-                    {row.InHospital}
-                  </td>
-                  <td
-                    style={{
-                      padding: "3px",
-                      border: "1px solid lightgray",
-                      color: "rgb(29, 240, 57)",
-                    }}
-                  >
-                    {row.WillAdminssion}
-                  </td>
-                  <td style={{ padding: "3px", border: "1px solid lightgray", color: "red" }}>
-                    {row.WillDischarged}
-                  </td>
-                  <td style={{ padding: "3px", border: "1px solid lightgray" }}>{row.EmptyBed}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            ))}
+          </tbody>
+        </table>
       </CardContent>
     </Card>
   );
